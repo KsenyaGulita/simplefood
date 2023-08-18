ea906e9ff05f516d96830b612d7d6a23ad282376
KsenyaGulita <ksenyagulita@gmail.com>
2023-08-18 21:17:57 +0300

    added changes for styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,8 @@
-$(function(){
-  
-  
-
-});
-
 $('.slider').slick({
   dots: true,
   arrows: true,
   prevArrow: '<button class="slider__arrow slider__arrow--prev" type="button"><svg class="slider__arrow-icon" width="18" height="18"><use xlink:href="images/sprite.svg#left-icon"></use></svg></button>',
-  nextArrow: '<button class="slider__arrow slider__arrow--next" type="button"><svg class="slider__arrow-icon" width="18" height="18"><use xlink:href="images/sprite.svg#right-icon"></use></svg></button>',
-  
-  
+  nextArrow: '<button class="slider__arrow slider__arrow--next" type="button"><svg class="slider__arrow-icon" width="18" height="18"><use xlink:href="images/sprite.svg#right-icon"></use></svg></button>',  
 });
 
 var mixer = mixitup('.popular-categories__catalog');
